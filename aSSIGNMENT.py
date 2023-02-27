a96ce11a720bc1c938159a4e59db75716dbae4d5
cmm25 <123402581+cmm25@users.noreply.github.com>
2023-02-27 19:02:20 +0300

    Add files via upload

--- aSSIGNMENT.py
+++ aSSIGNMENT.py
@@ -3,21 +3,31 @@ import pandas as pd
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy import linalg
-from math import pi,cos
-data= pd.read_table('weather.txt', delimiter=',')
-x=np.abs(data["Tmean"]).to_numpy()
-x_90=x[:90]
-y=data['day'].to_numpy()
-plt.plot(x_90,y_90,".")
-def line(t,a,b,c):
-    return a *np.cos(2*pi*t+b)+c
-popt, pcov=curve_fit(line, x_90, y_90)
-e=np.repeat(10.,90)
-popt, pcov=curve_fit(line, x_90,y_90, sigma=e)
+from math import pi, cos
+import streamlit as st
+
+st.title("MY PLOTS")
+data = pd.read_table('weather.txt', delimiter=',')
+x = np.abs(data["Tmean"]).to_numpy()
+x_90 = x[:90]
+y = data['day'].to_numpy()
+y_90=y[:90]
+plt.plot(x_90, y_90, ".")
+st.pyplot(plt.gcf())
+
+
+def line(t, a, b, c):
+    return a * np.cos(2*pi*t+b)+c
+
+
+popt, pcov = curve_fit(line, x_90, y_90)
+e = np.repeat(10., 90)
+popt, pcov = curve_fit(line, x_90, y_90, sigma=e)
 popt
 pcov
-print("b= ", popt[1], "+/-", pcov[1,1]**.5)
-print("a= ", popt[0], "+/-", pcov[0,0]**.5)
-plt.errorbar(x_90,y_90,yerr=e, fmt="none")
-xfine=np.linspace(0., 100., 100)
-plt.plot(xfine, line(xfine,popt[0], popt[1], popt[2]), 'r-')
+print("b= ", popt[1], "+/-", pcov[1, 1]**.5)
+print("a= ", popt[0], "+/-", pcov[0, 0]**.5)
+plt.errorbar(x_90, y_90, yerr=e, fmt="none")
+xfine = np.linspace(0., 100., 100)
+plt.plot(xfine, line(xfine, popt[0], popt[1], popt[2]), 'r-')
+st.pyplot(plt.gcf())
